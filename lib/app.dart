124d77fc40519929fc4d93932622bdc5cbc682dd
pranto372 <prantodeb62@gmail.com>
2023-11-16 20:11:39 +0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/Splash_Screen.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: Colors.white,
+          filled: true,
+          border: OutlineInputBorder(
+              borderSide: BorderSide.none
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide.none
+          ),
+        ),
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        primaryColor: Colors.green,
+        primarySwatch: Colors.green,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green,
+            padding: const EdgeInsets.symmetric(vertical: 10),
+          ),
+        )
+      ),
+    );
+  }
+}
